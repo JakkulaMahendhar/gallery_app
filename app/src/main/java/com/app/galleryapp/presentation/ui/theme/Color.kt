048623a37c9c7172cae173be4edb85a7f7f9b66b
Mahendhar <mahendharjakkula96@gmail.com>
2025-04-05 15:43:12 +0530

    added dependencies for dagger hilt, viewmodels, glide, unit testing

---
+++ app/src/main/java/com/app/galleryapp/presentation/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.app.galleryapp.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Colors
+val LightPrimary = Color(0xFFFFFFFF)
+val LightOnPrimary = Color(0xFF000000)
+val LightBackground = Color(0xFFFFFFFF)
+val LightOnBackground = Color(0xFF000000)
+
+// Dark Theme Colors
+val DarkPrimary = Color(0xFF121212)
+val DarkOnPrimary = Color(0xFFFFFFFF)
+val DarkBackground = Color(0xFF121212)
+val DarkOnBackground = Color(0xFFFFFFFF)
